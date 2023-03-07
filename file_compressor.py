c07ffe9d8527c1bbdaee13abf391c84512e5045d
KG <KeilGriesLP@gmail.com>
2023-03-06 21:32:02 -0500

    Added some error handling + converted python script into executable file.

--- file_compressor.py
+++ file_compressor.py
@@ -2,11 +2,11 @@ import PySimpleGUI as sg
 from zip_creator import make_archive
 
 label1 = sg.Text('Select files to compress:', background_color='black')
-input1 = sg.Input()
+input1 = sg.Input('')
 choose_button1 = sg.FilesBrowse('Choose', key='files')
 
 label2 = sg.Text('Select destination folder:', background_color='black')
-input2 = sg.Input()
+input2 = sg.Input('')
 choose_button2 = sg.FolderBrowse('Choose', key='folder')
 
 compress_button = sg.Button('Compress')
@@ -19,10 +19,12 @@ window = sg.Window("File Compressor", background_color='black',
 
 while True:
     event, values = window.read()
-    print(event, values)
-    filepaths = values['files'].split(';')
-    folder = values['folder']
-    make_archive(filepaths, folder)
-    window['output'].update(value='Compression completed!')
+    try:
+        filepaths = values['files'].split(';')
+        folder = values['folder']
+        make_archive(filepaths, folder)
+        window['output'].update(value='Compression completed!')
+    except AttributeError:
+        break
 
 window.close()
